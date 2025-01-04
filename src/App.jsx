46820d7d8f9fa4c5d21d85c3ce82cba5cf243055
Albin Baby <albinbabayme05@gmail.com>
2025-01-04 10:44:35 +0100

    static image added in public folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,29 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
 
+import ImageGallery from './components/ImageGallery'
 
+function App() {
+  /* create a array for static image */
+  const arrImg = [
+    {img : '../public/images/1.jpg'},
+    {img : '../public/images/2.jpg'},
+    {img : '../public/images/3.jpg'},
+    {img : '../public/images/4.jpg'},
+    {img : '../public/images/5.jpg'},
+    {img : '../public/images/6.jpg'},
+    {img : '../public/images/7.jpg'},
+    {img : '../public/images/8.jpg'},
+    {img : '../public/images/9.jpg'},
+    {img : '../public/images/10.jpg'},
+    {img : '../public/images/11.jpg'},
+    {img : '../public/images/12.jpg'},
+  ]
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+  return (
+    <>
+      <ImageGallery arrImg={arrImg}></ImageGallery>
+    </>
+    
+  )
+}
+
+export default App
